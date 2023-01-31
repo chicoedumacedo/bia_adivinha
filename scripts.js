b0203fee50498975bf733dd31400400dfcce6695
Eduardo Macedo <chicoedumacedo@gmail.com>
2023-01-31 17:02:00 -0300

    codigos para alteração de tela

--- scripts.js
+++ scripts.js
@@ -1,9 +1,37 @@
-//let tentativas = 0;
-//let numeroRandomico = Math.floor(Math.random() * 100) + 1;
-//let numeroCerto = numeroRandomico
-//console.log(numeroCerto)
+let sugestao = document.getElementById("sugestao")
+let bia = document.getElementById("bia")
+let ganhou = document.getElementById("ganhou")
+let perdeu = document.getElementById("perdeu")
+let tentativa = document.getElementById("tentativa")
 
-function enviar(){
-    var valorDigitado = document.getElementById("textinput").value;
-    console.log("Valor digitado " + valorDigitado);
-}
+let numeroRandomico = Math.floor(Math.random() * 100) + 1;
+let numeroCerto = numeroRandomico;
+let number = 0;
+
+console.log(numeroCerto)
+
+let tentativas = []
+
+tentativa.addEventListener('click', function () {
+
+    tentativas.push(Number(sugestao.value))
+
+    if (tentativas.includes(numeroCerto)) {
+        console.log("acertou")
+        ganhou.style.zIndex = 2
+        ganhou.style.display = "block"
+        perdeu.style.display = "none"
+        bia.style.display = "none"
+    } else {
+        console.log("errou")
+        perdeu.style.zIndex = 2
+        perdeu.style.display = "block"
+        ganhou.style.display = "none"
+        bia.style.display = "none"
+    }
+    console.log(tentativas)
+
+})
+
+
+
