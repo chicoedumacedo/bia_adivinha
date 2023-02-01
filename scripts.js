aba69b8d7708a5052cf1d07ca88379b78fb3777c
Eduardo Macedo <chicoedumacedo@gmail.com>
2023-01-31 23:11:51 -0300

    validação das tentativas, exibir array palpites na tela

--- scripts.js
+++ scripts.js
@@ -12,26 +12,55 @@ console.log(numeroCerto)
 
 let tentativas = []
 
-tentativa.addEventListener('click', function () {
+    tentativa.addEventListener('click', function () {
+
+    // if (tentativas.includes(sugestao.value) == true){
+    //     alert('Palpite Repetido')
+    //     console.log('teste',tentativas.includes(sugestao.value) == true)
+    // }else{
+    //     tentativas.push(Number(sugestao.value))  
+    // }
 
     tentativas.push(Number(sugestao.value))
+    
 
-    if (tentativas.includes(numeroCerto)) {
-        console.log("acertou")
-        ganhou.style.zIndex = 2
-        ganhou.style.display = "block"
-        perdeu.style.display = "none"
-        bia.style.display = "none"
-    } else {
-        console.log("errou")
-        perdeu.style.zIndex = 2
-        perdeu.style.display = "block"
-        ganhou.style.display = "none"
-        bia.style.display = "none"
+    if (sugestao.value > numeroRandomico){
+       alert('O numero digitado é maior') 
     }
+    if (sugestao.value < numeroRandomico){
+        alert('O numero digitado é menor') 
+    }
+
+    if (tentativas.length <= 10){
+        console.log('numero de tentativas menor que 10', tentativas.length)
+
+        if (tentativas.includes(numeroCerto)) {
+            console.log("acertou")
+            ganhou.style.zIndex = 2
+            ganhou.style.display = "block"
+            perdeu.style.display = "none"
+            bia.style.display = "none"
+            alert('Parabéns Você Ganhou')
+        } else {
+            console.log("errou")
+            perdeu.style.zIndex = 2
+            perdeu.style.display = "block"
+            ganhou.style.display = "none"
+            bia.style.display = "none"
+        }
+        
+    } else {
+        console.log('numero tentativas maior que 10', tentativas.length)
+        alert('Voce perdeu')
+        tentativas = []
+    }
+
     console.log(tentativas)
 
+    document.getElementById('resultado').innerHTML = tentativas;
 })
+
+
 
 
 
